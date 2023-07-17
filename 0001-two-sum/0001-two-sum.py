48f8b9f6253979ec49e2a6f4727c9f1c9e73c079
hema-keerthi-chakka <106919410+hema-keerthi-chakka@users.noreply.github.com>
2023-07-17 16:26:55 +0530

    Time: 41 ms (88.74%), Space: 14.3 MB (20.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        # HashTable approach
+        # HashTable approach - Complexity- Time:O(N), Space:O(N) 
         vals={}
         for i in range(len(nums)):
             pair=target-nums[i]
@@ -14,5 +14,17 @@ class Solution(object):
                 return [i,vals.get(pair)]
             else:
                 vals[nums[i]]=i
+                
+#         Brute Force: 
+#             iterate through each element, 
+#             compare every other element with the cur element.
+#             If they match return their indices
+#             Complexity: O(N^2)
+                
+#         Better approaches:
+            
+#         Sort + Binary Search for pair  O(NlogN)
+#         Sort + use 2 pointers to navigate to the right pair O(NlogN)
+            
                 
                 
