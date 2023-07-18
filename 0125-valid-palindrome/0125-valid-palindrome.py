9bab399a9b8573b250e28448bb6ea5432662c874
hema-keerthi-chakka <106919410+hema-keerthi-chakka@users.noreply.github.com>
2023-07-19 00:02:01 +0530

    Time: 31 ms (84.48%), Space: 13.9 MB (98.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,14 +5,32 @@ class Solution(object):
         :rtype: bool
         """
         
-        l = ''
-        for i in s:
-            if i.isalnum() :
-                l += i
-        l = l.lower()
-        if l == l[::-1]:
-            return True
-        else:
-            return False
+        # Time complexity: O(N) , space: O(N)
+        # l = ''
+        # for i in s:
+        #     if i.isalnum() :
+        #         l += i
+        # l = l.lower()
+        # if l == l[::-1]:
+        #     return True
+        # else:
+        #     return False
+        
+        #Time Complexity: O(N) , space: O(1)
+        i=0
+        j=len(s)-1
+        s=s.lower()
+        while i<=j:
+            if s[i].isalnum() and s[j].isalnum():
+                if s[i]!=s[j]:
+                    return False
+                i+=1
+                j-=1
+            if i<len(s) and (not s[i].isalnum()):
+                i+=1
+            if j>=0 and (not s[j].isalnum()):
+                j-=1
+        return True
+            
                 
             
