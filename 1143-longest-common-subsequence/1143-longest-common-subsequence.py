6d2b1f743070ee7d18e8d55e668be31aba15e080
hema-keerthi-chakka <106919410+hema-keerthi-chakka@users.noreply.github.com>
2023-09-08 19:57:56 +0530

    Time: 3579 ms (6.91%), Space: 198.7 MB (6.78%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -7,16 +7,15 @@ class Solution(object):
             if m==0 or n==0:
                 return 0
 
-            if text1[m-1] == text2[n-1]:
-                if (m-1,n-1) not in dp:
-                    dp[(m-1,n-1)] = lcs(m-1,n-1)
-                return 1 + dp[(m-1,n-1)]
+            if (m,n) in dp:
+                return dp[(m,n)]
             
+            if text1[m-1] == text2[n-1]:
+                dp[(m,n)]= 1+ lcs(m-1,n-1)
+
             else:
-                if (m,n-1) not in dp:
-                    dp[(m,n-1)] = lcs(m,n-1)
-                if (m-1,n) not in dp:
-                    dp[(m-1,n)] = lcs(m-1,n)
-                return max( dp[(m,n-1)] , dp[(m-1,n)] )
+                dp[(m,n)]= max( lcs(m,n-1) , lcs(m-1,n) )
+                
+            return dp[(m,n)]
         
         return lcs(len(text1),len(text2))
